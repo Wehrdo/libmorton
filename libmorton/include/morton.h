2abe3e2b7bdfff06aaea3ae9281e8e655425bb14
David Wehr <dawehr@iastate.edu>
2018-04-21 20:40:42 -0500

    Switched from uint_fast32_t to uint32_t.

--- libmorton/include/morton.h
+++ libmorton/include/morton.h
@@ -24,58 +24,58 @@
 
 // ENCODING
 #if defined(__BMI2__) || __AVX2__
-inline uint_fast32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
-	return m2D_e_BMI<uint_fast32_t, uint_fast16_t>(x, y);
+inline uint32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
+	return m2D_e_BMI<uint32_t, uint_fast16_t>(x, y);
 }
-inline uint_fast64_t morton2D_64_encode(const uint_fast32_t x, const uint_fast32_t y) {
-	return m2D_e_BMI<uint_fast64_t, uint_fast32_t>(x, y);
+inline uint64_t morton2D_64_encode(const uint32_t x, const uint32_t y) {
+	return m2D_e_BMI<uint64_t, uint32_t>(x, y);
 }
-inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
-	return m3D_e_BMI<uint_fast32_t, uint_fast16_t>(x, y, z);
+inline uint32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
+	return m3D_e_BMI<uint32_t, uint_fast16_t>(x, y, z);
 }
-inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z) {
-	return m3D_e_BMI<uint_fast64_t, uint_fast32_t>(x, y, z);
+inline uint64_t morton3D_64_encode(const uint32_t x, const uint32_t y, const uint32_t z) {
+	return m3D_e_BMI<uint64_t, uint32_t>(x, y, z);
 }
 #else
-__device__ __host__ inline uint_fast32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
-	return m2D_e_sLUT<uint_fast32_t, uint_fast16_t>(x, y);
+__device__ __host__ inline uint32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
+	return m2D_e_sLUT<uint32_t, uint_fast16_t>(x, y);
 }
-__device__ __host__ inline uint_fast64_t morton2D_64_encode(const uint_fast32_t x, const uint_fast32_t y) {
-	return m2D_e_sLUT<uint_fast64_t, uint_fast32_t>(x, y);
+__device__ __host__ inline uint64_t morton2D_64_encode(const uint32_t x, const uint32_t y) {
+	return m2D_e_sLUT<uint64_t, uint32_t>(x, y);
 }
-__device__ __host__ inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
-	return m3D_e_sLUT<uint_fast32_t, uint_fast16_t>(x, y, z);
+__device__ __host__ inline uint32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
+	return m3D_e_sLUT<uint32_t, uint_fast16_t>(x, y, z);
 }
-__device__ __host__ inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z) {
-	return m3D_e_sLUT<uint_fast64_t, uint_fast32_t>(x, y, z);
+__device__ __host__ inline uint64_t morton3D_64_encode(const uint32_t x, const uint32_t y, const uint32_t z) {
+	return m3D_e_sLUT<uint64_t, uint32_t>(x, y, z);
 }
 #endif
 
 // DECODING
 #if defined(__BMI2__) || __AVX2__
-inline void morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
-	m2D_d_BMI<uint_fast32_t, uint_fast16_t>(morton, x, y);
+inline void morton2D_32_decode(const uint32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
+	m2D_d_BMI<uint32_t, uint_fast16_t>(morton, x, y);
 }
-inline void morton2D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y) {
-	m2D_d_BMI<uint_fast64_t, uint_fast32_t>(morton, x, y);
+inline void morton2D_64_decode(const uint64_t morton, uint32_t& x, uint32_t& y) {
+	m2D_d_BMI<uint64_t, uint32_t>(morton, x, y);
 }
-inline void morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
-	m3D_d_BMI<uint_fast32_t, uint_fast16_t>(morton, x, y, z);
+inline void morton3D_32_decode(const uint32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
+	m3D_d_BMI<uint32_t, uint_fast16_t>(morton, x, y, z);
 }
-inline void morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z) {
-	m3D_d_BMI<uint_fast64_t, uint_fast32_t>(morton, x, y, z);
+inline void morton3D_64_decode(const uint64_t morton, uint32_t& x, uint32_t& y, uint32_t& z) {
+	m3D_d_BMI<uint64_t, uint32_t>(morton, x, y, z);
 }
 #else
-__device__ __host__ inline void morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
-	m2D_d_sLUT<uint_fast32_t, uint_fast16_t>(morton, x, y);
+__device__ __host__ inline void morton2D_32_decode(const uint32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
+	m2D_d_sLUT<uint32_t, uint_fast16_t>(morton, x, y);
 }
-__device__ __host__ inline void morton2D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y) {
-	m2D_d_sLUT<uint_fast64_t, uint_fast32_t>(morton, x, y);
+__device__ __host__ inline void morton2D_64_decode(const uint64_t morton, uint32_t& x, uint32_t& y) {
+	m2D_d_sLUT<uint64_t, uint32_t>(morton, x, y);
 }
-__device__ __host__ inline void morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
-	m3D_d_sLUT<uint_fast32_t, uint_fast16_t>(morton, x, y, z);
+__device__ __host__ inline void morton3D_32_decode(const uint32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
+	m3D_d_sLUT<uint32_t, uint_fast16_t>(morton, x, y, z);
 }
-__device__ __host__ inline void morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z) {
-	m3D_d_sLUT<uint_fast64_t, uint_fast32_t>(morton, x, y, z);
+__device__ __host__ inline void morton3D_64_decode(const uint64_t morton, uint32_t& x, uint32_t& y, uint32_t& z) {
+	m3D_d_sLUT<uint64_t, uint32_t>(morton, x, y, z);
 }
 #endif
